fcc6bb76c49c0c1ae6f2e6d0d213a7e8cf63a4cb
iagus <santiago@simplelogica.net>
2015-05-29 20:02:29 +0200

    Add column role_id to users (foreign key)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527192219) do
+ActiveRecord::Schema.define(version: 20150529173759) do
 
   create_table "resources", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -56,9 +56,12 @@ ActiveRecord::Schema.define(version: 20150527192219) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role_id",                limit: 4
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
 
+  add_foreign_key "users", "roles"
 end
