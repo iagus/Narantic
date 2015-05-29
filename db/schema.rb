840a761f7173826eb3c40658a8c2df4e80334279
iagus <santiago@simplelogica.net>
2015-05-29 23:47:23 +0200

    Schema app changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529173759) do
+ActiveRecord::Schema.define(version: 20150529210248) do
 
   create_table "resources", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 20150529173759) do
   end
 
   create_table "roles", force: :cascade do |t|
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.integer  "role",       limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "role",       limit: 255
   end
 
   create_table "tickets", force: :cascade do |t|
