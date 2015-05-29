1d828fc726f60b05da0c5216ee6f7f2e9622ac2d
iagus <santiago@simplelogica.net>
2015-05-29 18:39:07 +0200

    Fix devise after_sign_in and after_sign_out at app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for resource
-    redirect_to main_path
+    main_path
   end
 
   def after_sign_out_path_for resource
-    redirect_to root_path
+    root_path
   end
 
   protected
