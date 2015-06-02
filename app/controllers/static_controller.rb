f39bee395b4f480013079abded4ecbb33808282d
iagus <santiago@simplelogica.net>
2015-06-02 23:25:58 +0200

    Declare variables in index action to use them in view

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,8 @@
 class StaticController < ApplicationController
   before_action :authenticate_user!
   def index
+    @user = User.first
+    @ticket = Ticket.first
+    @resource = Resource.first
   end
 end
