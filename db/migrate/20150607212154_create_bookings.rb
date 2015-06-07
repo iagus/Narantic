7f57b154bcb4468ce69151f16791f10c3f9b309c
iagus <santiago@simplelogica.net>
2015-06-08 00:31:02 +0200

    Bookings table migration

---
+++ db/migrate/20150607212154_create_bookings.rb
@@ -0,0 +1,10 @@
+class CreateBookings < ActiveRecord::Migration
+  def change
+    create_table :bookings do |t|
+      t.references :resource, index: true, foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
