89e02124c15bd86324935c821366f11748fb47f6
iagus <santiago@simplelogica.net>
2015-06-10 00:23:03 +0200

    Replace jQuery code by disabled in input

--- app/assets/javascripts/ready.js
+++ app/assets/javascripts/ready.js
@@ -7,8 +7,4 @@ $(document).ready(function() {
   $(function() {
     $('form').find('select, input:file, input:checkbox, input:radio').formikation();
   });
-
-  //// Disable certain inputs
-  $('#booking_user_id').prop('disabled', true);
-  $('#ticket_user_id').prop('disabled', true);
 })
